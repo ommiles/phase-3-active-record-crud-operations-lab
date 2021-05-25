95d5e3138eae959cab4a5ae74849c8bfd8c3b36e
OM Miles <miles@ommiles.com>
2021-05-24 18:31:51 -0700

    lab complete

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,7 @@
 class Movie < ActiveRecord::Base
+
+    def movie
+        # Movie.all.find {|movie| movie.id == movie_id}
+        # Movie.where {movie_id: self.id}
+    end
 end
---
+++ db/migrate/20210524230627_create_movies.rb
@@ -0,0 +1,15 @@
+class CreateMovies < ActiveRecord::Migration[5.2]
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.integer :release_date
+      t.string :director
+      t.string :lead
+      t.boolean :in_theaters
+    end
+  end
+end
+
+# def change
+#   add_column :movies, :x, :string
+# end
